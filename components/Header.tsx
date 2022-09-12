c7dce0b04ee78bef25a0f94f724ea7f004b3695c
Nel Fernandes <46218259+imNel@users.noreply.github.com>
2022-09-12 23:23:33 +0800

    Started work on styling homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,27 @@
+import Link from "next/link";
+import style from "../styles/Header.module.scss";
+
 const Header = () => {
-  return <>this is my header</>
-}
+  return (
+    <nav className={style.main}>
+      <div className={style.btn}>
+        <svg>
+          <line x1="0" y1="0%" x2="100%" y2="0%" strokeWidth="2" />
+          <line x1="0" y1="50%" x2="100%" y2="50%" strokeWidth="1" />
+          <line x1="0" y1="100%" x2="100%" y2="100%" strokeWidth="2" />
+        </svg>
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="projects">
+        <a>Projects</a>
+      </Link>
+      <Link href="blog">
+        <a>Blog</a>
+      </Link>
+    </nav>
+  );
+};
 
 export default Header;
