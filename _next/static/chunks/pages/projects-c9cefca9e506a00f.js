84d0642ee9790b8dc7f26bf84f0745008b846530
imNel <imNel@users.noreply.github.com>
2022-09-17 16:12:25 +0000

    Deploying to gh-pages from @ imNel/imnel.github.io@47e5f4b25cf4497b448409bffb0070efe45281d8 🚀

---
+++ _next/static/chunks/pages/projects-c9cefca9e506a00f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327],{7670:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/projects",function(){return t(9240)}])},9240:function(n,u,t){"use strict";t.r(u);var r=t(5893),c=function(){return(0,r.jsx)(r.Fragment,{})};u.default=c}},function(n){n.O(0,[774,888,179],function(){return n(n.s=7670)}),_N_E=n.O()}]);
